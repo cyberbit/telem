8e5b063e96c666bc7dace94ebe9c9b8b58d929d6
cyberbit <dj.marcolesco@gmail.com>
2023-11-24 23:27:19 +0000

    📦 improve module and dependency packages
    - add message when run from shell (idiot)
    - update package.path for vendors to run from telem/vendor folder

--- src/telem/vendor/init.lua
+++ src/telem/vendor/init.lua
@@ -1,3 +1,5 @@
+if package.path:find('telem/vendor') == nil then package.path = package.path .. ';telem/vendor/?;telem/vendor/?.lua;telem/vendor/?/init.lua' end
+
 local ecnet2 = require 'ecnet2'
 local random = require 'ccryptolib.random'
 
