1be3ac6a7feaa9055a233aee7e098c14db8b1b12
cyberbit <dj.marcolesco@gmail.com>
2024-05-14 01:59:34 +0000

    📦 update for Basalt 1.7

--- src/telem/init.lua
+++ src/telem/init.lua
@@ -1,9 +1,9 @@
 -- Telem by cyberbit
 -- MIT License
--- Version 0.5.0
+-- Version 0.5.1
 
 local _Telem = {
-    _VERSION = '0.5.0',
+    _VERSION = '0.5.1',
     util = require 'telem.lib.util',
     input = require 'telem.lib.input',
     output = require 'telem.lib.output',
