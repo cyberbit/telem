12ad7c3f4451268403a1593e52c62154f2d5b615
cyberbit <git@cyberbit.dev>
2024-05-21 12:38:31 -0500

    bump version

--- src/telem/init.lua
+++ src/telem/init.lua
@@ -1,9 +1,9 @@
 -- Telem by cyberbit
 -- MIT License
--- Version 0.5.1
+-- Version 0.5.2
 
 local _Telem = {
-    _VERSION = '0.5.1',
+    _VERSION = '0.5.2',
     util = require 'telem.lib.util',
     input = require 'telem.lib.input',
     output = require 'telem.lib.output',
