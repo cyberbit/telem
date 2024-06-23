16ece6ade878cc59868d58089fcba76f55530eae
cyberbit <git@cyberbit.dev>
2024-06-23 21:34:35 +0000

    📦 update mechanical pipe and pressurized tube adapters

--- src/telem/lib/input.lua
+++ src/telem/lib/input.lua
@@ -1,32 +1,34 @@
-local prefixInput = 'telem.lib.input.'
-local prefixMek = prefixInput .. 'mekanism.'
+local function prefixInput(target) return 'telem.lib.input.' .. target end
+local function prefixMek(target) return prefixInput('mekanism.' .. target) end
 
 return {
-    helloWorld              = require (prefixInput .. 'HelloWorldInputAdapter'),
-    custom                  = require (prefixInput .. 'CustomInputAdapter'),
+    helloWorld              = require (prefixInput('HelloWorldInputAdapter')),
+    custom                  = require (prefixInput('CustomInputAdapter')),
 
     -- storage
-    itemStorage             = require (prefixInput .. 'ItemStorageInputAdapter'),
-    fluidStorage            = require (prefixInput .. 'FluidStorageInputAdapter'),
-    refinedStorage          = require (prefixInput .. 'RefinedStorageInputAdapter'),
-    meStorage               = require (prefixInput .. 'MEStorageInputAdapter'),
+    itemStorage             = require (prefixInput('ItemStorageInputAdapter')),
+    fluidStorage            = require (prefixInput('FluidStorageInputAdapter')),
+    refinedStorage          = require (prefixInput('RefinedStorageInputAdapter')),
+    meStorage               = require (prefixInput('MEStorageInputAdapter')),
 
     -- machinery
     mekanism = {
-        bioGenerator        = require (prefixMek .. 'BioGeneratorInputAdapter'),
-        chemicalTank        = require (prefixMek .. 'ChemicalTankInputAdapter'),
-        digitalMiner        = require (prefixMek .. 'DigitalMinerInputAdapter'),
-        dynamicTank         = require (prefixMek .. 'DynamicTankInputAdapter'),
-        fissionReactor      = require (prefixMek .. 'FissionReactorInputAdapter'),
-        fusionReactor       = require (prefixMek .. 'FusionReactorInputAdapter'),
-        gasGenerator        = require (prefixMek .. 'GasGeneratorInputAdapter'),
-        inductionMatrix     = require (prefixMek .. 'InductionMatrixInputAdapter'),
-        industrialTurbine   = require (prefixMek .. 'IndustrialTurbineInputAdapter'),
-        isotopicCentrifuge  = require (prefixMek .. 'IsotopicCentrifugeInputAdapter'),
-        laser               = require (prefixMek .. 'LaserInputAdapter'),
-        laserAmplifier      = require (prefixMek .. 'LaserAmplifierInputAdapter'),
+        bioGenerator        = require (prefixMek('BioGeneratorInputAdapter')),
+        chemicalTank        = require (prefixMek('ChemicalTankInputAdapter')),
+        digitalMiner        = require (prefixMek('DigitalMinerInputAdapter')),
+        dynamicTank         = require (prefixMek('DynamicTankInputAdapter')),
+        fissionReactor      = require (prefixMek('FissionReactorInputAdapter')),
+        fusionReactor       = require (prefixMek('FusionReactorInputAdapter')),
+        gasGenerator        = require (prefixMek('GasGeneratorInputAdapter')),
+        inductionMatrix     = require (prefixMek('InductionMatrixInputAdapter')),
+        industrialTurbine   = require (prefixMek('IndustrialTurbineInputAdapter')),
+        isotopicCentrifuge  = require (prefixMek('IsotopicCentrifugeInputAdapter')),
+        laser               = require (prefixMek('LaserInputAdapter')),
+        laserAmplifier      = require (prefixMek('LaserAmplifierInputAdapter')),
+        mechanicalPipe      = require (prefixMek('MechanicalPipeInputAdapter')),
+        pressurizedTube     = require (prefixMek('PressurizedTubeInputAdapter')),
     },
 
     -- modem
-    secureModem = require (prefixInput .. 'SecureModemInputAdapter'),
+    secureModem = require (prefixInput('SecureModemInputAdapter')),
 }
--- src/telem/lib/input/mekanism/MechanicalPipeInputAdapter.lua
+++ src/telem/lib/input/mekanism/MechanicalPipeInputAdapter.lua
@@ -1,22 +1,22 @@
 local o = require 'telem.lib.ObjectModel'
 local t = require 'telem.lib.util'
+local fn = require 'telem.vendor'.fluent.fn
 
-local InputAdapter      = require 'telem.lib.InputAdapter'
-local Metric            = require 'telem.lib.Metric'
-local MetricCollection  = require 'telem.lib.MetricCollection'
+local BaseMekanismInputAdapter = require 'telem.lib.input.mekanism.BaseMekanismInputAdapter'
 
-local MechanicalPipeInputAdapter = o.class(InputAdapter)
+local MechanicalPipeInputAdapter = o.class(BaseMekanismInputAdapter)
 MechanicalPipeInputAdapter.type = 'MechanicalPipeInputAdapter'
 
 function MechanicalPipeInputAdapter:constructor (peripheralName, categories)
-    self:super('constructor')
+    self:super('constructor', peripheralName)
 
     -- TODO this will be a configurable feature later
-    self.prefix = 'mekmechpipe:'
+    self.prefix = 'mekpipe:'
 
     -- TODO make these constants
     local allCategories = {
         'basic',
+        'transfer',
     }
 
     if not categories then
@@ -27,41 +27,19 @@ function MechanicalPipeInputAdapter:constructor (peripheralName, categories)
         self.categories = categories
     end
 
-    -- boot components
-    self:setBoot(function ()
-        self.components = {}
+    self.queries = {
+        basic = {
+            filled_percentage   = fn():call('getFilledPercentage')
+        },
+        transfer = {
+            buffer              = fn():call('getBuffer'):get('amount'):div(1000):fluid(),
+            capacity            = fn():call('getCapacity'):div(1000):fluid(),
+            needed              = fn():call('getNeeded'):div(1000):fluid(),
+        }
+    }
 
-        self:addComponentByPeripheralID(peripheralName)
-    end)()
-end
-
-function MechanicalPipeInputAdapter:read ()
-    self:boot()
-
-    local source, tube = next(self.components)
-
-    local metrics = MetricCollection()
-
-    local loaded = {}
-
-    for _,v in ipairs(self.categories) do
-        -- skip, already loaded
-        if loaded[v] then
-            -- do nothing
-
-        -- Literally all we have lmao
-        elseif v == 'basic' then
-            metrics:insert(Metric{ name = self.prefix .. 'buffer', value = tube.getBuffer() / 1000, unit = "B", source = source })
-            metrics:insert(Metric{ name = self.prefix .. 'capacity', value = tube.getCapacity() / 1000, unit = "B", source = source }) -- might error might not, no clue!
-            metrics:insert(Metric{ name = self.prefix .. 'needed', value = tube.getNeeded() / 1000, unit = "B", source = source })
-            metrics:insert(Metric{ name = self.prefix .. 'filled_percentage', value = tube.getFilledPercentage(), unit = nil, source = source })
-        end
-
-        loaded[v] = true
-    end
-
-    return metrics
+    -- TODO only supports energy and direction
+    -- self:withGenericMachineQueries()
 end
 
 return MechanicalPipeInputAdapter
-
