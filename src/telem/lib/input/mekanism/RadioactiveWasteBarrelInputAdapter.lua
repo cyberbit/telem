c8ae9106ba417de1f224012592c95c80d7a542bc
cyberbit <git@cyberbit.dev>
2024-07-07 16:41:25 +0000

    📦 update radioactive waste barrel and seismic vibrator adapters for newer mek versions

--- src/telem/lib/input/mekanism/RadioactiveWasteBarrelInputAdapter.lua
+++ src/telem/lib/input/mekanism/RadioactiveWasteBarrelInputAdapter.lua
@@ -8,8 +8,34 @@ local RadioactiveWasteBarrelInputAdapter = base.mintAdapter('RadioactiveWasteBar
 function RadioactiveWasteBarrelInputAdapter:beforeRegister ()
     self.prefix = 'mekwastebarrel:'
 
-    -- TODO Mekanism 10.2.5 does not have proper support for waste barrels,
-    -- so the queries have not yet been built. This will be updated in the future.
+    local _, component = next(self.components)
+    local supported = peripheral.getType(component) == 'radioactiveWasteBarrel'
+
+    if not supported then
+        error('Radioactive Waste Barrel is not supported in this version of Mekanism')
+    end
+
+    self.queries = {
+        basic = {
+            filled_percentage   = fn():call('getFilledPercentage'),
+        },
+        storage = {
+            stored              = fn():call('getStored'):get('amount'):div(1000):fluid(),
+            capacity            = fn():call('getCapacity'):div(1000):fluid(),
+            needed              = fn():call('getNeeded'):div(1000):fluid(),
+        },
+    }
+
+    self.storageQueries = {
+        fn():call('getStored'):transform(function (v)
+            return { Metric{ name = v.name, value = v.amount / 1000, unit = 'B' } }
+        end)
+    }
+
+    -- getDirection
+    
+    -- TODO only supports direction and comparator
+    -- self:withGenericMachineQueries()
 end
 
 return RadioactiveWasteBarrelInputAdapter
--- src/telem/lib/input/mekanism/SeismicVibratorInputAdapter.lua
+++ src/telem/lib/input/mekanism/SeismicVibratorInputAdapter.lua
@@ -1,11 +1,15 @@
 local fn = require 'telem.vendor'.fluent.fn
 
-local base = require 'telem.lib.input.mekanism.BaseMekanismInputAdapter'
+local base      = require 'telem.lib.input.mekanism.BaseMekanismInputAdapter'
+local Metric    = require 'telem.lib.Metric'
 
 local SeismicVibratorInputAdapter = base.mintAdapter('SeismicVibratorInputAdapter')
 
 function SeismicVibratorInputAdapter:beforeRegister ()
     self.prefix = 'mekseismic:'
+
+    local _, component = next(self.components)
+    local supportsStorage = type(component.getColumnAt) == 'function'
 
     self.queries = {
         basic = {
@@ -18,8 +22,49 @@ function SeismicVibratorInputAdapter:beforeRegister ()
         },
     }
 
-    -- TODO Mekanism 10.2.5 does not have proper support for seismic vibrators,
-    -- so only energy is supported for now. This will be updated in the future.
+    -- Mekanism 10.3+ only
+    if supportsStorage then
+        self.storageQueries = {
+            fn()
+                :transform(function (v)
+                    local columns = {}
+            
+                    local queueChunks, chunkSize = {}, 16
+                    local chunkIdx = 1
+            
+                    for x = 0, 15 do
+                        for z = 0, 15 do
+                            queueChunks[chunkIdx] = queueChunks[chunkIdx] or {}
+            
+                            table.insert(queueChunks[chunkIdx], function ()
+                                columns['c' .. x .. ',' .. z] = v.getColumnAt(x, z) or false
+                            end)
+            
+                            if #queueChunks[chunkIdx] >= chunkSize then
+                                chunkIdx = chunkIdx + 1
+                            end
+                        end
+                    end
+            
+                    for _, chunk in ipairs(queueChunks) do
+                        parallel.waitForAll(table.unpack(chunk))
+                    end
+            
+                    return columns
+                end)
+                :flatten()
+                :pluck('block')
+                :reject(function (_, v)
+                    return v == 'minecraft:air' or v == 'minecraft:void_air'
+                end)
+                :mapValues(function (v)
+                    return { name = v, value = 1 }
+                end)
+                :sum('value', 'name')
+                :map(function (k, v) return Metric{ name = k, value = v, unit = 'item' } end)
+                :values()
+        }
+    end
     
     -- TODO does not support comparator
     -- self:withGenericMachineQueries()
