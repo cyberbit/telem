fa756b828a6a358c2b20f3ee2e2c2437a2006135
cyberbit <git@cyberbit.dev>
2024-07-08 04:01:21 +0000

    📦 update bin adapter to standards

--- src/telem/lib/input/mekanism/BinInputAdapter.lua
+++ src/telem/lib/input/mekanism/BinInputAdapter.lua
@@ -9,7 +9,7 @@ function BinInputAdapter:beforeRegister ()
     self.prefix = 'mekbin:'
 
     self.queries = {
-        storage = {
+        basic = {
             stored      = fn():call('getStored'):get('count'):with('unit', 'item'),
             capacity    = fn():call('getCapacity'):with('unit', 'item'),
         },
@@ -17,6 +17,10 @@ function BinInputAdapter:beforeRegister ()
 
     self.storageQueries = {
         fn():call('getStored'):transform(function (v)
+            if v.count == 0 then
+                return {}
+            end
+            
             return { Metric{ name = v.name, value = v.count, unit = 'item' } }
         end)
     }
