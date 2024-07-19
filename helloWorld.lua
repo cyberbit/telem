dd3452a1c9f9fc63274f6fb8b9534e985d2c5848
cyberbit <git@cyberbit.dev>
2024-07-18 20:56:36 -0500

    Delete helloWorld.lua

--- helloWorld.lua
+++
@@ -1,8 +0,0 @@
-package.path = package.path .. ';src/?.lua;src/?/init.lua'
-
-local telem = require 'dist.telem'
-
-local backplane = telem.backplane():debug(true)
-    :addInput('hello_in', telem.input.helloWorld(123))
-    :addOutput('hello_out', telem.output.helloWorld())
-    :cycleEvery(5)()
