79c94e1b23c89ef05fc1117eb0ee619b91c792bd
cyberbit <git@cyberbit.dev>
2026-07-19 05:08:46 +0000

    📦 add parallel input reads to backplane

--- src/telem/lib/Backplane.lua
+++ src/telem/lib/Backplane.lua
@@ -172,13 +172,38 @@ function Backplane:cycle()
 
     self:dlog('Backplane:cycle :: reading inputs...')
 
-    -- read inputs
+    -- queue all input read requests
+    local inputResults = {}
+    local inputTasks = {}
+
     for _, key in ipairs(self.inputKeys) do
         local input = self.inputs[key]
 
-        self:dlog('Backplane:cycle ::  - ' .. key)
+        table.insert(inputTasks, function ()
+            self:dlog('Backplane:cycle ::  - ' .. key)
 
-        local results = {pcall(input.read, input)}
+            inputResults[key] = {pcall(input.read, input)}
+        end)
+    end
+
+    -- TODO determine appropriate chunk size
+    -- 8 Mekanism Fusion Reactor inputs generate up to 34*8 = 272 peripheral calls
+    local chunkSize = 8
+
+    -- TODO create chunks in previous loop
+    for i = 1, #inputTasks, chunkSize do
+        local chunk = {}
+
+        for j = i, math.min(i + chunkSize - 1, #inputTasks) do
+            table.insert(chunk, inputTasks[j])
+        end
+        
+        parallel.waitForAll(table.unpack(chunk))
+    end
+
+    -- process results in original order
+    for _, key in ipairs(self.inputKeys) do
+        local results = inputResults[key]
 
         if not table.remove(results, 1) then
             t.log('Input fault for "' .. key .. '":')
@@ -187,7 +212,7 @@ function Backplane:cycle()
             local inputCollection = table.remove(results, 1)
 
             -- process input middleware
-            local processedInputCollection = self:processMiddleware(input.middlewares, inputCollection)
+            local processedInputCollection = self:processMiddleware(self.inputs[key].middlewares, inputCollection)
 
             for _,v in ipairs(processedInputCollection.metrics) do
                 -- attach adapter name
